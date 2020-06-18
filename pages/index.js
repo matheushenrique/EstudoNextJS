4ba312995ffc6479956c3f0d679dde6ccc75348c
matheushenrique <matheus.henriquepr@gmail.com>
2020-06-17 22:27:39 -0300

    correcao de path para arquivo estatico

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const Home = () => (
     <Head>
       <title>Home</title>
     </Head>
-    <img src="../static/matrix.jpg" alt="teste" width="200" />
+    <img src="/static/matrix.jpg" alt="teste" width="200" />
 
     <h1>Hello</h1>
     <Link href="/users">
